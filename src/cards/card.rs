89e58747eac68b3afdb673dc447a9cf9ea131083
Joshua Rose <joshuarose@gmx.com>
2023-11-28 14:44:13 +1100

    feat: add cards, rank, suit, deck
    * additional refactors to existing files as well

---
+++ src/cards/card.rs
@@ -0,0 +1,74 @@
+//! A card in the game of poker. This is of course excluding the jokers and the wild cards.
+//!
+//! Cards have a suit and a rank. The suit is one of the four suits in a deck of cards. The rank
+//! is one of the 13 ranks in a deck of cards. The rank is also used to determine the value of the
+//! card.
+
+use super::deck::Deck;
+use super::rank::Rank;
+use super::suit::Suit;
+
+/// A card in the game of poker.
+#[derive(PartialEq, Debug)]
+pub struct Card {
+    /// The suit of the card.
+    suit: Suit,
+    /// The rank of the card.
+    rank: Rank,
+}
+
+impl Card {
+    /// Creates a new card.
+    pub fn new(suit: Suit, rank: Rank) -> Self {
+        Self { suit, rank }
+    }
+
+    /// Return a vector of all the cards in a deck of cards.
+    pub fn all() -> Vec<Card> {
+        // create a new deck of cards
+        let mut cards = Deck::new();
+
+        // return the cards
+        cards.cards
+    }
+
+    fn get_suit(&self) -> Suit {
+        self.suit
+    }
+
+    fn get_rank(&self) -> Rank {
+        self.rank
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let card = Card::new(Suit::Spades, Rank::Ace);
+        assert_eq!(card.get_suit(), Suit::Spades);
+        assert_eq!(card.get_rank(), Rank::Ace);
+    }
+
+    #[test]
+    fn test_get_suit() {
+        let card = Card::new(Suit::Spades, Rank::Ace);
+        assert_eq!(card.get_suit(), Suit::Spades);
+    }
+
+    #[test]
+    fn test_get_rank() {
+        let card = Card::new(Suit::Spades, Rank::Ace);
+        assert_eq!(card.get_rank(), Rank::Ace);
+    }
+
+    #[test]
+    fn test_all() {
+        let cards = Card::all();
+        for (i, card) in Card::all().iter().enumerate() {
+            assert_eq!(*card, cards[i]);
+        }
+    }
+}
---
+++ src/cards/hand.rs
@@ -0,0 +1,150 @@
+//! In poker, the core part, or at least one of the main parts,
+//! is what 'hand'  you have. this is important becuase it can determine who wins and loses.
+//!
+//! this module contains things that pertain to the functionality of the aforementioned subject
+//! matter.
+
+/// A struct representing a hand of cards in a game of poker.
+///
+/// # Example
+/// ```rust,ignore,no_run
+/// use poker::Hand;
+///
+/// let hand = Hand::new();
+///
+/// if hand.is_flush() {
+///    println!("You have a flush!");
+///    // Do something
+/// }
+/// // I'm sure you can think of something better to do with this.
+/// ```
+pub enum Hand {
+    /// A Royal Flush is a straight flush with the highest cards.
+    RoyalFlush,
+    /// A Straight Flush is a flush with cards in a sequence.
+    StraightFlush,
+    /// A Four of a Kind is four cards of the same rank.
+    FourOfAKind,
+    /// A Full House is three cards of the same rank and two cards of the same rank.
+    FullHouse,
+    /// A Flush is five cards of the same suit.
+    Flush,
+    /// A Straight is five cards in a sequence.
+    Straight,
+    /// A Three of a Kind is three cards of the same rank.
+    ThreeOfAKind,
+    /// A Two Pair is two cards of the same rank and two cards of the same rank.
+    TwoPair,
+    /// A One Pair is two cards of the same rank.
+    OnePair,
+    /// A High Card is a hand that does not fit into any of the other categories.
+    HighCard,
+}
+
+impl PartialEq for Hand {
+    /// Checks if two hands are equal.
+    ///
+    /// # Example
+    ///
+    /// ```rust,ignore,no_run
+    /// use poker::Hand;
+    ///
+    /// let hand = Hand::new();
+    ///
+    /// if hand == Hand::RoyalFlush {
+    ///   println!("You have a royal flush!");
+    ///   // Do something
+    /// }
+    fn eq(&self, other: &Hand) -> bool {
+        match self {
+            Hand::RoyalFlush => matches!(other, Hand::RoyalFlush),
+            Hand::StraightFlush => matches!(other, Hand::StraightFlush),
+            Hand::FourOfAKind => matches!(other, Hand::FourOfAKind),
+            Hand::FullHouse => matches!(other, Hand::FullHouse),
+            Hand::Flush => matches!(other, Hand::Flush),
+            Hand::Straight => matches!(other, Hand::Straight),
+            Hand::ThreeOfAKind => matches!(other, Hand::ThreeOfAKind),
+            Hand::TwoPair => matches!(other, Hand::TwoPair),
+            Hand::OnePair => matches!(other, Hand::OnePair),
+            Hand::HighCard => matches!(other, Hand::HighCard),
+        }
+    }
+}
+
+impl Default for Hand {
+    /// Creates a new hand.
+    ///
+    /// # Example
+    /// ```rust,ignore,no_run
+    /// use poker::Hand;
+    ///
+    /// let hand = Hand::new();
+    /// ```
+    fn default() -> Hand {
+        Hand::new()
+    }
+}
+
+impl Hand {
+    /// Creates a new hand.
+    ///
+    /// # Example
+    /// ```rust,ignore,no_run
+    /// use poker::Hand;
+    ///
+    /// let hand = Hand::new();
+    /// ```
+    pub fn new() -> Hand {
+        Hand::HighCard
+    }
+
+    /// Checks if the hand is a Royal Flush.
+    pub fn is_royal_flush(&self) -> bool {
+        self == &Hand::RoyalFlush
+    }
+
+    /// Checks if the hand is a Straight Flush.
+    pub fn is_straight_flush(&self) -> bool {
+        self == &Hand::StraightFlush
+    }
+
+    /// Checks if the hand is a Four of a Kind.
+    pub fn is_four_of_a_kind(&self) -> bool {
+        self == &Hand::FourOfAKind
+    }
+
+    /// Checks if the hand is a Full House.
+    pub fn is_full_house(&self) -> bool {
+        self == &Hand::FullHouse
+    }
+
+    /// Checks if the hand is a Flush.
+    pub fn is_flush(&self) -> bool {
+        self == &Hand::Flush
+    }
+
+    /// Checks if the hand is a Straight.
+    pub fn is_straight(&self) -> bool {
+        self == &Hand::Straight
+    }
+
+    /// Checks if the hand is a Three of a Kind.
+    pub fn is_three_of_a_kind(&self) -> bool {
+        self == &Hand::ThreeOfAKind
+    }
+
+    /// Checks if the hand is a Two Pair.
+    pub fn is_two_pair(&self) -> bool {
+        self == &Hand::TwoPair
+    }
+
+    /// Checks if the hand is a One Pair.
+    pub fn is_one_pair(&self) -> bool {
+        self == &Hand::OnePair
+    }
+
+    /// Checks if the hand is a High Card.
+    pub fn is_high_card(&self) -> bool {
+        self == &Hand::HighCard
+    }
+}
